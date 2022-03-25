d986e0fed43f61a8a6e7fd86b57d01373dc2fc18
Vladimir Lukinov <lukinov.vladimir.1988@mail.ru>
2022-03-25 18:50:06 +0300

    Homeworks1.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,32 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let arr; 
+  let d = b ** 2 - 4 * a * c;
+  let x1;
+  let x2;
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
+  if (d === 0) {
+    x1 = -b / (2 * a); //1 корень
+    arr = [x1];
+  } else if (d < 0) {
+    arr = []; // выводит пустой массив
+  } else {
+    x1 = (-b + Math.sqrt(d)) / (2 * a); //2 корня
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr = [x1, x2];
+  }
+
+  return arr; 
+ 
+};
+
+
+function calculateTotalMortgage(percent, contribution, amount, date) { 
+
   let totalAmount;
-
+ 
   // код для задачи №2 писать здесь
+
+
 
   return totalAmount;
 }
