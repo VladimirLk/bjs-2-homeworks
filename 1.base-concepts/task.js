aeafa29cf1faf69536d5398f3d5307f373e25697
Vladimir Lukinov <lukinov.vladimir.1988@mail.ru>
2022-03-28 13:12:24 +0300

    homework by function

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,5 @@
 function solveEquation(a, b, c) {
-  let arr; 
+  let arr = [] ; 
   let d = b ** 2 - 4 * a * c;
   let x1;
   let x2;
@@ -7,12 +7,14 @@ function solveEquation(a, b, c) {
   if (d === 0) {
     x1 = -b / (2 * a); //1 корень
     arr = [x1];
-  } else if (d < 0) {
-    arr = []; // выводит пустой массив
-  } else {
+  // } else if (d < 0) {
+  //   arr = []; // выводит пустой массив
+  } else if (d > 0 ){
     x1 = (-b + Math.sqrt(d)) / (2 * a); //2 корня
     x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr = [x1, x2];
+  }else{
+    arr = [];
   }
 
   return arr; 
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,65 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
   // Ваш код
+  let min, max, sum, avg;
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  avg = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+    sum += arr[i]
+  }
+  avg = sum / arr.length;
+  avg = Number(avg.toFixed(2))
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
   // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
+  let max = -Infinity;
+  // Ваш код
   // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i])
+    }
+  }
+
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  
+  for(let i = 0; i < arr.length; i++){
+
+    if (arr[i] < min){
+      min = arr[i];
+    };
+
+    if (arr[i] > max){
+      max = arr[i];
+    };
+  }
+  return max - min;
 }
+
