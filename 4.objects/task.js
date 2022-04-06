e978a2afccb307181b7b91a36ec3b493c5f84f02
Vladimir Lukinov <lukinov.vladimir.1988@mail.ru>
2022-04-06 19:16:16 +0300

    homeworks_4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,34 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+   // Ваш код
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+    //ваш код
 }
 
 // ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  this.marks = mark;
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((sum, current) => sum + current) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+}
